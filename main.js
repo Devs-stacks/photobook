a8923d64b1c5cf6e4438f16c7e4566c49714f96c
Gem Akinbo <127473139+G24-gem@users.noreply.github.com>
2025-08-19 18:01:33 +0100

    Update main.js, 19 Aug  to fix the responsiveness and enable the popovers

--- main.js
+++ main.js
@@ -6,42 +6,50 @@ var currentStudentData = studentData[currentpage]
 console.log(currentStudentData)
 
 
+// Enable all popovers
+document.addEventListener("DOMContentLoaded", () => {
+  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  popoverTriggerList.map(el => new bootstrap.Popover(el))
+})
+
+
 function stu(name, phonenumber, short, whatsapp, fb, ig, sn, tktk) {
-	return `
-<div class="card mb-3" id="card" style="max-width: 540px; margin-top: 50px; border-color: purple;">
+  return `
+<div class="card mb-3" style="max-width: 540px; margin-top: 50px; border-color: purple;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="Profile Pisc/${name.toLowerCase()}.jpg" class="img-fluid rounded-start" alt="...">
+      <img src="Profile Pisc/${name.toLowerCase()}.jpg" class="img-fluid rounded-start" alt="${name}">
     </div>
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title text-white">${name}</h5>
-        <p class="card-text text-white" id="new">${phonenumber}</p>
+        <p class="card-text text-white">${phonenumber}</p>
         <p class="card-text text-white"><small class="text-body-secondary">${short}</small></p>
-    <div class="container-fluid social">
-      <button type="button" class="btn btn-dark" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="bottom popover">
-      <i class="bi bi-whatsapp">${whatsapp}</i>
-      </button>  
-      <button type="button" class="btn btn-dark" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="bottom popover">
-      <i class="bi bi-snapchat">${sn}</i>
-      </button>  
-      <button type="button" class="btn btn-dark" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="bottom popover">
-      <i class="bi bi-facebook">${fb}</i>
-      </button>  
-      <button type="button" class="btn btn-dark" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="bottom popover">
-      <i class="bi bi-instagram">${ig}</i>
-      </button>  
-      <button type="button" class="btn btn-dark" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="bottom popover">
-      <i class="bi bi-tiktok">${tktk}</i>
-      </button>  
-   
-     </div>
+        <div class="d-flex flex-wrap gap-2 social">
+          <button type="button" class="btn btn-dark" data-bs-toggle="popover" data-bs-content="${whatsapp}">
+            <i class="bi bi-whatsapp"></i>
+          </button>
+          <button type="button" class="btn btn-dark" data-bs-toggle="popover" data-bs-content="${sn}">
+            <i class="bi bi-snapchat"></i>
+          </button>
+          <button type="button" class="btn btn-dark" data-bs-toggle="popover" data-bs-content="${fb}">
+            <i class="bi bi-facebook"></i>
+          </button>
+          <button type="button" class="btn btn-dark" data-bs-toggle="popover" data-bs-content="${ig}">
+            <i class="bi bi-instagram"></i>
+          </button>
+          <button type="button" class="btn btn-dark" data-bs-toggle="popover" data-bs-content="${tktk}">
+            <i class="bi bi-tiktok"></i>
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </div>
-	`
+  `
 }
+
+
 
 let collation = [];
 currentStudentData.forEach((item, i) => {
@@ -55,3 +63,4 @@ document.querySelector("#cardz").innerHTML = sentCollation
 var elem = document.querySelector("#new")
 //console.log(elem)
 elem.innerHTML = studentData[0]
+
